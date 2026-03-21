5f79801a8e237705e8b1e01fe1bbbdc322f0a772
DeVerl Stokes <deverl.stokes@warrantyhub.com>
2026-03-21 08:56:31 -0600

    Added a litle output to the error script.

--- generate_error.sh
+++ generate_error.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
+
+for i in {1..2}
+do
+    l="$i : $(base64 /dev/urandom 2> /dev/null | head -c 60)"
+    echo $l
+    sleep 1
+done
+
+echo "Oops, something went wrong!"
+
+
 exit 1
 
